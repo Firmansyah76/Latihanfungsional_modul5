6914e42770f13b9aea6b2554c957a5e51e971d17
Firmansyah76 <127119694+Firmansyah76@users.noreply.github.com>
2023-11-30 12:05:48 +0700

    Add files via upload

---
+++ Percobaan1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])  # Mengubah nilai x
+ypoints = np.array([3, 10])  # Mengubah nilai y
+
+plt.plot(xpoints, ypoints)
+plt.show()
